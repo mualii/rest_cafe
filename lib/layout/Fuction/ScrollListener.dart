d33089d95ceadc2b8c47c46777d89b385aad2833
MohammedAlyGouda <moaly995@gmail.com>
2021-09-23 10:37:40 +0200

    editing in scrool view

--- lib/layout/Fuction/ScrollListener.dart
+++ lib/layout/Fuction/ScrollListener.dart
@@ -11,12 +11,39 @@ class ScrollListener extends ChangeNotifier {
   ScrollListener.initialise(ScrollController controller,
       [double height = 100]) {
     controller.addListener(() {
-      final current = controller.offset;
-      bottom += _last - current;
-      if (bottom <= -height) bottom = -height;
-      if (bottom >= 0) bottom = 0;
+      double current = controller.offset;
+      print("current" + current.toString());
+      print("userScrollDirection =" +
+          controller.position.userScrollDirection.toString());
+
+      bottom += _last - (current);
+      print("bottom" + bottom.toString());
+      print("- height = " + (-height).toString());
+
+      if (bottom < 0) {
+        bottom = -height;
+        // bottom -= 20;
+        print("انا نازل");
+      }
+      // if (current == 0.0) {
+      //   bottom = 0;
+      //   print("$bottom");
+      // }
+      if (controller.position.userScrollDirection.toString() ==
+          "ScrollDirection.forward") {
+        current = 0;
+        bottom = 0;
+      }
       _last = current;
-      if (bottom <= 0 && bottom >= -height) notifyListeners();
+      if (bottom <= 0 && bottom >= -height) {
+        //  print("bottom bf");
+        //  print(bottom);
+        //   print("bottom af");
+        //  bottom -= 100;
+        //    print(bottom);
+
+        notifyListeners();
+      }
     });
   }
 }
